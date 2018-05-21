b400ee3f56ab51b2de545fa458503c42531ea711
Julian van de Groep <me@flash.moe>
2018-05-22 01:05:25 +0200

    SQL is black magic

---
+++ public/forum/posting.php
@@ -0,0 +1,127 @@
+<?php
+use Misuzu\Database;
+use Misuzu\Net\IPAddress;
+
+require_once __DIR__ . '/../../misuzu.php';
+
+if (!$app->hasActiveSession()) {
+    header('Location: /');
+    return;
+}
+
+$postRequest = $_SERVER['REQUEST_METHOD'] === 'POST';
+
+$db = Database::connection();
+
+// ORDER OF CHECKING
+//  - $postId non-zero: enter quote mode
+//  - $topicId non-zero: enter reply mode
+//  - $forumId non-zero: enter create mode
+//  - all zero: enter explode mode
+if ($postRequest) {
+    $topicId = max(0, (int)($_POST['post']['topic'] ?? 0));
+    $forumId = max(0, (int)($_POST['post']['forum'] ?? 0));
+} else {
+    $postId = max(0, (int)($_GET['p'] ?? 0));
+    $topicId = max(0, (int)($_GET['t'] ?? 0));
+    $forumId = max(0, (int)($_GET['f'] ?? 0));
+}
+
+if (!empty($postId)) {
+    $getPost = $db->prepare('
+        SELECT `post_id`, `topic_id`, `user_id`, `post_text`
+        FROM `msz_forum_posts`
+        WHERE `post_id` = :post_id
+    ');
+    $getPost->bindValue('post_id', $postId);
+    $post = $getPost->execute() ? $getPost->fetch() : false;
+
+    if (isset($post['topic_id'])) { // should automatic cross-quoting be a thing? if so, check if $topicId is < 1 first
+        $topicId = (int)$post['topic_id'];
+    }
+}
+
+if (!empty($topicId)) {
+    $getTopic = $db->prepare('
+        SELECT `topic_id`, `forum_id`
+        FROM `msz_forum_topics`
+        WHERE `topic_id` = :topic_id
+    ');
+    $getTopic->bindValue('topic_id', $topicId);
+    $topic = $getTopic->execute() ? $getTopic->fetch() : false;
+
+    if (isset($topic['forum_id'])) {
+        $forumId = (int)$topic['forum_id'];
+    }
+}
+
+if (!empty($forumId)) {
+    $getForum = $db->prepare('
+        SELECT `forum_id`
+        FROM `msz_forum_categories`
+        WHERE `forum_id` = :forum_id
+    ');
+    $getForum->bindValue('forum_id', $forumId);
+    $forum = $getForum->execute() ? $getForum->fetch() : false;
+}
+
+if ($postRequest) {
+    $createPost = $db->prepare('
+        INSERT INTO `msz_forum_posts`
+            (`topic_id`, `forum_id`, `user_id`, `post_ip`, `post_text`)
+        VALUES
+            (:topic_id, :forum_id, :user_id, INET6_ATON(:post_ip), :post_text)
+    ');
+
+    if (isset($topic)) {
+        $bumpTopic = $db->prepare('
+            UPDATE `msz_forum_topics`
+            SET `topic_bumped` = NOW()
+            WHERE `topic_id` = :topic_id
+        ');
+        $bumpTopic->bindValue('topic_id', $topic['topic_id']);
+        $bumpTopic->execute();
+    } else {
+        $createTopic = $db->prepare('
+            INSERT INTO `msz_forum_topics`
+                (`forum_id`, `user_id`, `topic_title`)
+            VALUES
+                (:forum_id, :user_id, :topic_title)
+        ');
+        $createTopic->bindValue('forum_id', $forum['forum_id']);
+        $createTopic->bindValue('user_id', $app->getUserId());
+        $createTopic->bindValue('topic_title', $_POST['post']['title']);
+        $createTopic->execute();
+        $topicId = (int)$db->lastInsertId();
+    }
+
+    $createPost->bindValue('topic_id', $topicId);
+    $createPost->bindValue('forum_id', $forum['forum_id']);
+    $createPost->bindValue('user_id', $app->getUserId());
+    $createPost->bindValue('post_ip', IPAddress::remote()->getString());
+    $createPost->bindValue('post_text', $_POST['post']['text']);
+    $createPost->execute();
+    $postId = $db->lastInsertId();
+
+    header("Location: /forum/topic.php?p={$postId}#p{$postId}");
+    return;
+}
+
+echo '<form method="post" action="/forum/posting.php">';
+
+if (isset($topic)) {
+    echo "<input type='hidden' name='post[topic]' value='{$topic['topic_id']}'>";
+} else {
+    echo "<input type='hidden' name='post[forum]' value='{$forum['forum_id']}'>";
+    echo '<input type="text" name="post[title]"><br>';
+}
+
+echo '<textarea name="post[text]">';
+
+if (isset($post)) {
+    echo '[quote=' . $post['user_id'] . ']' . $post['post_text'] . '[/quote]';
+}
+
+echo '</textarea><br>
+        <button>Submit</button>
+    </form>';
--- public/forum/topic.php
+++ public/forum/topic.php
@@ -13,23 +13,44 @@ $postsRange = max(min((int)($_GET['r'] ?? 10), 25), 5);
 
 // find topic id
 if ($topicId < 1 && $postId > 0) {
-    $getTopicId = $db->prepare('
-        SELECT `topic_id`
-        FROM `msz_forum_posts`
-        WHERE `post_id` = :post_id
+    $getPostInfo = $db->prepare('
+        SELECT
+        :post_id as `target_post_id`,
+        (
+            SELECT `topic_id`
+            FROM `msz_forum_posts`
+            WHERE `post_id` = `target_post_id`
+        ) as `target_topic_id`,
+        (
+            SELECT COUNT(`post_id`)
+            FROM `msz_forum_posts`
+            WHERE `topic_id` = `target_topic_id`
+            AND `post_id` < `target_post_id`
+            ORDER BY `post_id`
+        ) as `preceeding_post_count`
     ');
-    $getTopicId->bindValue('post_id', $postId);
-    $topicId = $getTopicId->execute() ? (int)$getTopicId->fetchColumn() : 0;
+    $getPostInfo->bindValue('post_id', $postId);
+    $postInfo = $getPostInfo->execute() ? $getPostInfo->fetch() : false;
+
+    if ($postInfo) {
+        $topicId = (int)$postInfo['target_topic_id'];
+        $postsOffset = floor($postInfo['preceeding_post_count'] / $postsRange) * $postsRange;
+    }
 }
 
 $getTopic = $db->prepare('
     SELECT
         t.`topic_id`, t.`forum_id`, t.`topic_title`, t.`topic_type`, t.`topic_status`,
         (
-            SELECT MIN(p.`post_id`)
-            FROM `msz_forum_posts` as p
-            WHERE p.`topic_id` = t.`topic_id`
-        ) as `topic_first_post_id`
+            SELECT MIN(`post_id`)
+            FROM `msz_forum_posts`
+            WHERE `topic_id` = t.`topic_id`
+        ) as `topic_first_post_id`,
+        (
+            SELECT COUNT(`post_id`)
+            FROM `msz_forum_posts`
+            WHERE `topic_id` = t.`topic_id`
+        ) as `topic_post_count`
     FROM `msz_forum_topics` as t
     WHERE t.`topic_id` = :topic_id
     AND t.`topic_deleted` IS NULL
@@ -45,14 +66,32 @@ if (!$topic) {
 
 $getPosts = $db->prepare('
     SELECT
-        `post_id`, `post_text`, `post_created`,
-        `topic_id`
-    FROM `msz_forum_posts`
+        p.`post_id`, p.`post_text`, p.`post_created`,
+        p.`topic_id`,
+        u.`user_id` as `poster_id`,
+        u.`username` as `poster_name`,
+        u.`created_at` as `poster_joined`,
+        COALESCE(r.`role_colour`, CAST(0x40000000 AS UNSIGNED)) as `poster_colour`
+    FROM `msz_forum_posts` as p
+    LEFT JOIN `msz_users` as u
+    ON u.`user_id` = p.`user_id`
+    LEFT JOIN `msz_roles` as r
+    ON r.`role_id` = u.`display_role`
     WHERE `topic_id` = :topic_id
     AND `post_deleted` IS NULL
+    ORDER BY `post_id`
+    LIMIT :offset, :take
 ');
 $getPosts->bindValue('topic_id', $topic['topic_id']);
+$getPosts->bindValue('offset', $postsOffset);
+$getPosts->bindValue('take', $postsRange);
 $posts = $getPosts->execute() ? $getPosts->fetchAll() : [];
+
+if (!$posts) {
+    http_response_code(404);
+    echo $templating->render('errors.404');
+    return;
+}
 
 $lastParent = $topic['forum_id'];
 $breadcrumbs = [];
@@ -81,4 +120,6 @@ echo $templating->render('forum.topic', [
     'topic_breadcrumbs' => $breadcrumbs,
     'topic_info' => $topic,
     'topic_posts' => $posts,
+    'topic_offset' => $postsOffset,
+    'topic_range' => $postsRange,
 ]);
