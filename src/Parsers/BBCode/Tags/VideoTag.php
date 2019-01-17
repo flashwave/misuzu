2eceb64c50be8d15bc11d18b9577564638cbd343
Julian van de Groep <me@flash.moe>
2019-01-17 03:11:03 +0100

    Fixed youtu.be links not embedding.

--- src/Parsers/BBCode/Tags/VideoTag.php
+++ src/Parsers/BBCode/Tags/VideoTag.php
@@ -5,17 +5,34 @@ use Misuzu\Parsers\BBCode\BBCodeTag;
 
 final class VideoTag extends BBCodeTag
 {
-    private const YOUTUBE_REGEX = '#^https?://(?:www\.)?youtu(?:be\.(?:[a-z]{2,63})|\.be|\be-nocookie\.com)/(?:.*?)v=([a-zA-Z0-9_-]+)#si';
+    private const YOUTUBE_URL_REGEX = '#^(?:www\.)?youtube(?:-nocookie)?\.(?:[a-z]{2,63})$#u';
+    private const YOUTUBE_EMBED = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/%s?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
     public function parseText(string $text): string
     {
         return preg_replace_callback(
             '#\[video\]((?:https?:\/\/).+?)\[/video\]#',
             function ($matches) {
-                if (preg_match(self::YOUTUBE_REGEX, $matches[1], $ytMatches)) {
-                    return '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/'
-                        . $ytMatches[1]
-                        . '?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+                $url = parse_url($matches[1]);
+
+                if (empty($url['scheme']) || !in_array(mb_strtolower($url['scheme']), ['http', 'https'], true)) {
+                    return $matches[0];
+                }
+
+                $url['host'] = mb_strtolower($url['host']);
+
+                // support youtube playlists?
+
+                if ($url['host'] === 'youtu.be' || $url['host'] === 'www.youtu.be') {
+                    return sprintf(self::YOUTUBE_EMBED, $url['path']);
+                }
+
+                if (!empty($url['query']) && ($url['path'] ?? '') === '/watch' && preg_match(self::YOUTUBE_URL_REGEX, $url['host'])) {
+                    parse_str(html_entity_decode($url['query']), $ytQuery);
+
+                    if (!empty($ytQuery['v']) && preg_match('#^([a-zA-Z0-9_-]+)$#u', $ytQuery['v'])) {
+                        return sprintf(self::YOUTUBE_EMBED, $ytQuery['v']);
+                    }
                 }
 
                 $mediaUrl = proxy_media_url($matches[1]);
