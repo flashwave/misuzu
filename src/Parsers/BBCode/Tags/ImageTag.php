7490f838c8bcd5469b2806cab13baab039f1c396
Julian van de Groep <me@flash.moe>
2019-03-09 19:02:16 +0100

    Set a max-height on bbcode pictures.

--- src/Parsers/BBCode/Tags/ImageTag.php
+++ src/Parsers/BBCode/Tags/ImageTag.php
@@ -9,7 +9,7 @@ final class ImageTag extends BBCodeTag
     {
         return preg_replace_callback("/\[img\]((?:https?:\/\/).+?)\[\/img\]/", function ($matches) {
             $mediaUrl = proxy_media_url($matches[1]);
-            return sprintf('<img src="%s" alt="%s" style="max-width:100%%;max-height:100%%;">', $mediaUrl, $matches[1]);
+            return sprintf('<img src="%s" alt="%s" style="max-width:100%%;max-height:900px;">', $mediaUrl, $matches[1]);
         }, $text);
     }
 }
