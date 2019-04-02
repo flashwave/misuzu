a3e74b68066e536aaa9c19570235b322a7e0c4b0
Julian van de Groep <me@flash.moe>
2019-04-02 21:45:29 +0200

    Added more validation to audio tag.

--- src/Parsers/BBCode/Tags/AudioTag.php
+++ src/Parsers/BBCode/Tags/AudioTag.php
@@ -10,6 +10,14 @@ final class AudioTag extends BBCodeTag
         return preg_replace_callback(
             '#\[audio\]((?:https?:\/\/).+?)\[/audio\]#',
             function ($matches) {
+                $url = parse_url($matches[1]);
+
+                if (empty($url['scheme']) || !in_array(mb_strtolower($url['scheme']), ['http', 'https'], true)) {
+                    return $matches[0];
+                }
+
+                //$url['host'] = mb_strtolower($url['host']);
+
                 $mediaUrl = url_proxy_media($matches[1]);
                 return "<audio controls src='{$mediaUrl}'></audio>";
             },
