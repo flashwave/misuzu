df701c6939ee4597893836d78b38ba13f701b5b4
Julian van de Groep <me@flash.moe>
2019-08-14 21:46:19 +0200

    Added missing database migration for config.

---
+++ database/2019_08_14_194510_create_config_table.php
@@ -0,0 +1,20 @@
+<?php
+namespace Misuzu\DatabaseMigrations\CreateConfigTable;
+
+use PDO;
+
+function migrate_up(PDO $conn): void
+{
+    $conn->exec("
+        CREATE TABLE `msz_config` (
+            `config_name`   VARCHAR(100)    NOT NULL COLLATE 'utf8mb4_bin',
+            `config_value`  BLOB            NOT NULL DEFAULT '',
+            PRIMARY KEY (`config_name`)
+        ) COLLATE='utf8mb4_bin' ENGINE=InnoDB;
+    ");
+}
+
+function migrate_down(PDO $conn): void
+{
+    $conn->exec("DROP TABLE `msz_config`;");
+}
