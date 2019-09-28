2bedd42be1354ec10bf349a9ef3467934f8c422a
Julian van de Groep <me@flash.moe>
2019-09-29 01:44:48 +0200

    Added fetchObject wrappers.

--- src/Database/DatabaseStatement.php
+++ src/Database/DatabaseStatement.php
@@ -8,6 +8,7 @@ class DatabaseStatement {
     public $pdo;
     public $stmt;
     private $isQuery;
+    private $hasExecuted = false;
 
     public function __construct(PDOStatement $stmt, PDO $pdo, bool $isQuery) {
         $this->stmt = $stmt;
@@ -21,11 +22,24 @@ class DatabaseStatement {
     }
 
     public function execute(array $params = []): bool {
+        if($this->hasExecuted)
+            return true;
+        $this->hasExecuted = true;
+
         return count($params) ? $this->stmt->execute($params) : $this->stmt->execute();
     }
 
     public function executeGetId(array $params = []): int {
+        if($this->hasExecuted)
+            return true;
+        $this->hasExecuted = true;
+
         return $this->execute($params) ? $this->pdo->lastInsertId() : 0;
+    }
+
+    public function reset(): DatabaseStatement {
+        $this->hasExecuted = false;
+        return $this;
     }
 
     public function fetch($default = []) {
@@ -41,5 +55,25 @@ class DatabaseStatement {
     public function fetchColumn(int $num = 0, $default = null) {
         $out = $this->isQuery || $this->execute() ? $this->stmt->fetchColumn($num) : false;
         return $out ? $out : $default;
+    }
+
+    public function fetchObject(string $className = 'stdClass', ?array $args = null, $default = null) {
+        $out = false;
+
+        if($this->isQuery || $this->execute()) {
+            $out = $args === null ? $this->fetchObject($className) : $this->fetchObject($className, $args);
+        }
+
+        return $out !== false ? $out : $default;
+    }
+
+    public function fetchObjects(string $className = 'stdClass', ?array $args = null): array {
+        $objects = [];
+
+        while(($object = $this->fetchObject($className, $args, false)) !== false) {
+            $objects[] = $object;
+        }
+
+        return $objects;
     }
 }
