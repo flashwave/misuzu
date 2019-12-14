564a6bf4907039d822ba7ed8eba9ca5ffd7415c2
Julian van de Groep <me@flash.moe>
2019-12-14 02:22:00 +0100

    Make session token auth easier to implement.

--- src/Http/Handlers/SockChatHandler.php
+++ src/Http/Handlers/SockChatHandler.php
@@ -209,7 +209,8 @@ final class SockChatHandler extends Handler {
             if(user_password_verify_db($authInfo->user_id, mb_substr($authInfo->token, 5)))
                 $userId = $authInfo->user_id;
         } elseif($authMethod === 'SESS:') { // IMPROVE THIS
-            user_session_start($authInfo->user_id, mb_substr($authInfo->token, 5));
+            $tokenData = user_session_cookie_unpack(mb_substr($authInfo->token, 5), true));
+            user_session_start($authInfo->user_id, $tokenData['token']);
 
             if(user_session_active())
                 $userId = user_session_current('user_id');
