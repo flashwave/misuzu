1d43aa3b6972a61eebee3b8f8a06a75266376fd9
flashwave <me@flash.moe>
2020-05-18 21:40:21 +0000

    Fixed a couple oversights.

--- assets/js/misuzu/perms.js
+++ assets/js/misuzu/perms.js
@@ -8,8 +8,8 @@ Misuzu.Perms.check = function(section, value) {
 
 // Comment permissions
 Misuzu.Perms.prototype.canCreateComment      = Misuzu.Perms.check('comments', 0x01);
-Misuzu.Perms.prototype.canDeleteOwnComment   = Misuzu.Perms.check('comments', 0x0C);
-Misuzu.Perms.prototype.canDeleteAnyComment   = Misuzu.Perms.check('comments', 0x08);
-Misuzu.Perms.prototype.canLockCommentSection = Misuzu.Perms.check('comments', 0x10);
-Misuzu.Perms.prototype.canPinComment         = Misuzu.Perms.check('comments', 0x20);
-Misuzu.Perms.prototype.canVoteOnComment      = Misuzu.Perms.check('comments', 0x40);
+Misuzu.Perms.prototype.canDeleteOwnComment   = Misuzu.Perms.check('comments', 0x08 | 0x10);
+Misuzu.Perms.prototype.canDeleteAnyComment   = Misuzu.Perms.check('comments', 0x10);
+Misuzu.Perms.prototype.canLockCommentSection = Misuzu.Perms.check('comments', 0x20);
+Misuzu.Perms.prototype.canPinComment         = Misuzu.Perms.check('comments', 0x40);
+Misuzu.Perms.prototype.canVoteOnComment      = Misuzu.Perms.check('comments', 0x80);
